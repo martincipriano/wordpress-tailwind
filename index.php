24c780a52c04d9c1141937f2f2f21994b0c3340d
Jose Martin Cipriano <martin.c@scopicsoftware.com>
2023-12-14 23:06:01 +0800

    Include the header to the default template

--- index.php
+++ index.php
@@ -1,14 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="<?php bloginfo('charset'); ?>">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title><?php wp_title( '|', true, 'right' ); ?></title>
-
-  <?php wp_head(); ?>
-</head>
-<body>
-  <?php wp_body_open(); ?>
+<?php get_header(); ?>
 
   <div class="container mx-auto">
     Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque velit dicta ut illo obcaecati sapiente deleniti magni iusto aliquam tenetur suscipit eos inventore, debitis aspernatur ad vero, fuga in enim.
