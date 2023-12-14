862b999b91e4a513ac1d05917c94b0ef05c53d38
Jose Martin Cipriano <martin.c@scopicsoftware.com>
2023-12-14 23:30:13 +0800

    Create a base header style

--- header-navigation.php
+++ header-navigation.php
@@ -1,9 +1,14 @@
-<header>
-  <?= is_front_page() ? '<h1>' : '<div>' ?>
-    <a href="<?= site_url() ?>">Logo</a>
-  <?= is_front_page() ? '</h1>' : '</div>' ?>
-
-
-
-  <nav></nav>
+<header class="bg-gray-50 border-b px-5">
+  <div class="container flex items-center justify-between mx-auto">
+    <?= is_front_page() ? '<h1>' : '<div>' ?>
+      <a href="<?= site_url() ?>">Logo</a>
+    <?= is_front_page() ? '</h1>' : '</div>' ?>
+    <nav>
+      <ul class="flex">
+        <li><a class="block px-5 py-3" href="">Home</a></li>
+        <li><a class="block px-5 py-3" href="">Services</a></li>
+        <li><a class="block px-5 py-3" href="">Contact Us</a></li>
+      </ul>
+    </nav>
+  </div>
 </header>
